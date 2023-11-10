d528ef19398bab3fed0ab3d0aeaab492277cdd48
Martin Alfaro <50346244+alfaromartino@users.noreply.github.com>
2023-11-09 22:05:51 -0700

    update

--- assets/PAGES/8a_allocations/code/allCode.jl
+++ assets/PAGES/8a_allocations/code/allCode.jl
@@ -79,7 +79,7 @@ foo(x) = x .* x
  
  x      = [1,2,3]
 
-foo(x) = x[1:2]                 # ONE allocation, since ranges don't allocates (but 'x[1:2]' itself does)
+foo(x) = x[1:2]                 # ONE allocation, since ranges don't allocate (but 'x[1:2]' itself does)
 
 @btime foo(ref($x)) 
  
--- assets/PAGES/8a_allocations/code/region10.jl
+++ assets/PAGES/8a_allocations/code/region10.jl
@@ -1,5 +1,5 @@
 x      = [1,2,3]
 
-foo(x) = x[1:2]                 # ONE allocation, since ranges don't allocates (but 'x[1:2]' itself does)
+foo(x) = x[1:2]                 # ONE allocation, since ranges don't allocate (but 'x[1:2]' itself does)
 
 @btime foo(ref($x))
