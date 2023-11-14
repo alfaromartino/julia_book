bcf9dbfe3a58e240d4dd66f7f9f49be770df48cb
Martin Alfaro <50346244+alfaromartino@users.noreply.github.com>
2023-11-13 17:51:00 -0700

    update

--- assets/PAGES/8b3_reductions/code/allCode.jl
+++ assets/PAGES/8b3_reductions/code/allCode.jl
@@ -126,21 +126,21 @@ foo(x) = prod(a -> 2 * a, x)
  using Random; Random.seed!(123)       #setting the seed for reproducibility #hide
 x = rand(100); y = rand(100)
 
-foo(x,y) = sum(a -> (a[1] * a[2]), zip(x,y))
+foo(x,y) = sum(a -> a[1] * a[2], zip(x,y))
 
 @btime foo(ref($x), ref($y)) 
  
  using Random; Random.seed!(123)       #setting the seed for reproducibility #hide
 x = rand(100); y = rand(100)
 
-foo(x,y) = maximum(a -> (a[1] * a[2]), zip(x,y))
+foo(x,y) = maximum(a -> a[1] * a[2], zip(x,y))
 
 @btime foo(ref($x), ref($y)) 
  
  using Random; Random.seed!(123)       #setting the seed for reproducibility #hide
 x = rand(100); y = rand(100)
 
-foo(x,y) = prod(a -> (a[1] * a[2]), zip(x,y))
+foo(x,y) = prod(a -> a[1] * a[2], zip(x,y))
 
 @btime foo(ref($x), ref($y)) 
  
