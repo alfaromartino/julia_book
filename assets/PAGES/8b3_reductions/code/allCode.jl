672f5347c447d5b67f4b9d78e6468b50f6199109
Martin Alfaro <50346244+alfaromartino@users.noreply.github.com>
2023-11-13 17:42:13 -0700

    update

--- assets/PAGES/8b3_reductions/code/allCode.jl
+++ assets/PAGES/8b3_reductions/code/allCode.jl
@@ -144,30 +144,4 @@ foo(x,y) = prod(a -> (a[1] * a[2]), zip(x,y))
 
 @btime foo(ref($x), ref($y)) 
  
- using Random; Random.seed!(123)       #setting the seed for reproducibility #hide
-x = rand(100)
-
-our_mean(x) = mapreduce(a -> a/length(x), +, x)
-@btime our_mean(ref($x))
-
-our_mean(x) = mapreduce(identity, +, x) / length(x)
-@btime our_mean(ref($x))
-
-import Statistics: mean
-@btime mean(ref($x))
-
-our_weighted_mean(x,y) = mapreduce(x-> x[1]*x[2], +, zip(x,y))
-@btime our_weighted_mean(ref($x), ref($y))
-
-our_weighted_mean(x,y) = reduce(+, map(x-> x[1]*x[2], zip(x,y)))
-@btime our_weighted_mean(ref($x), ref($y))
-
-our_weighted_mean(x,y) = reduce(+, map((a,b)-> a*b, x,y))
-@btime our_weighted_mean(ref($x), ref($y))
-
-our_weighted_mean(x,y) = mapreduce(splat(*), +, zip(x,y))
-@btime our_weighted_mean(ref($x), ref($y))
-
-@btime mean(ref($x), weights(ref($y))) 
- 
  
