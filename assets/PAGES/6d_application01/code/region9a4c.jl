fe02a02177d1c85ba1b713691e21e38bd8fe158c
Martin Alfaro <50346244+alfaromartino@users.noreply.github.com>
2024-04-11 16:11:21 -0600

    update

--- assets/PAGES/6d_application01/code/region9a4c.jl
+++ assets/PAGES/6d_application01/code/region9a4c.jl
@@ -1,3 +1,3 @@
-days_to_consider = (1, 10, 25)
+days_to_consider = (1, 10, 25)      # days where the videos were posted
 is_day           = in.(eachindex(views_per_video), Ref(days_to_consider))
 specific_days    = stats_subset(views_per_video, pay_per_views, is_day)
