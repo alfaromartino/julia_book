06d21ccbd861da9a1212bb2241398d5653f839c0
Martin Alfaro <50346244+alfaromartino@users.noreply.github.com>
2024-04-11 16:13:39 -0600

    update

--- assets/PAGES/6d_application01/code/allCode.jl
+++ assets/PAGES/6d_application01/code/allCode.jl
@@ -149,7 +149,7 @@ notviral         = stats_subset(views_per_video, pay_per_views, is_notviral)
  
 print_compact(notviral) #hide
  
-days_to_consider = (1, 10, 25)      # days where the videos were posted
+days_to_consider = (1, 10, 25)      # days when the videos were posted
 is_day           = in.(eachindex(views_per_video), Ref(days_to_consider))
 specific_days    = stats_subset(views_per_video, pay_per_views, is_day)
  
