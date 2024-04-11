4d59c7ca142a0d8b28fc9cc5c82cc9d26afc468b
Martin Alfaro <50346244+alfaromartino@users.noreply.github.com>
2024-04-11 16:00:25 -0600

    update

--- assets/PAGES/6d_application01/code/allCode.jl
+++ assets/PAGES/6d_application01/code/allCode.jl
@@ -137,23 +137,21 @@ function stats_subset(views_per_video, pay_per_views, condition)
     return (; nrvideos, views, pay)
 end
  
-#for virals
 viral_threshold  = 100
 is_viral         = (views_per_video .≥ viral_threshold)
 viral            = stats_subset(views_per_video, pay_per_views, is_viral)
-
-#for non-virals
-is_notviral      = .!(is_viral)      # '!' is negating a boolean value, which we then broadcast
-notviral         = stats_subset(views_per_video, pay_per_views, is_notviral)
-
-# videos created in specific days
-days_to_consider = (1, 10, 25)
-is_day           = in.(eachindex(views_per_video), Ref(days_to_consider))
-specific_days    = stats_subset(views_per_video, pay_per_views, is_day)
  
 print_compact(viral) #hide
  
+viral_threshold  = 100
+is_notviral      = .!(is_viral)      # '!' is negating a boolean value, which we then broadcast
+notviral         = stats_subset(views_per_video, pay_per_views, is_notviral)
+ 
 print_compact(notviral) #hide
+ 
+days_to_consider = (1, 10, 25)
+is_day           = in.(eachindex(views_per_video), Ref(days_to_consider))
+specific_days    = stats_subset(views_per_video, pay_per_views, is_day)
  
 print_compact(specific_days) #hide
  
--- assets/PAGES/6d_application01/code/region9a4.jl
+++ assets/PAGES/6d_application01/code/region9a4.jl
@@ -1,13 +1,3 @@
-#for virals
 viral_threshold  = 100
 is_viral         = (views_per_video .≥ viral_threshold)
 viral            = stats_subset(views_per_video, pay_per_views, is_viral)
-
-#for non-virals
-is_notviral      = .!(is_viral)      # '!' is negating a boolean value, which we then broadcast
-notviral         = stats_subset(views_per_video, pay_per_views, is_notviral)
-
-# videos created in specific days
-days_to_consider = (1, 10, 25)
-is_day           = in.(eachindex(views_per_video), Ref(days_to_consider))
-specific_days    = stats_subset(views_per_video, pay_per_views, is_day)
