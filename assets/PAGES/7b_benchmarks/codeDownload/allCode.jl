8dce276b9b974f837a4ef220fc094dd9b88f81fe
alfaromartino <alfaromartino@gmail.com>
2024-10-04 20:55:36 -0600

    update

--- assets/PAGES/7b_benchmarks/codeDownload/allCode.jl
+++ assets/PAGES/7b_benchmarks/codeDownload/allCode.jl
@@ -5,6 +5,16 @@
     # this means that benchmarking a function `foo(x)` makes use of `foo($x)`
 using BenchmarkTools
 
+# The following defines the macro `@fast_btime foo($x)`
+    # `@fast_btime` is equivalent to `@btime` but substantially faster
+    # if you want to use it, you should replace `@btime` with `@fast_btime`
+    # by default, if `@fast_btime` doesn't provide allocations, it means there are none
+using Chairmarks
+macro fast_btime(ex)
+    return quote
+        display(@b $ex)
+    end
+end
 
 ############################################################################
 #
