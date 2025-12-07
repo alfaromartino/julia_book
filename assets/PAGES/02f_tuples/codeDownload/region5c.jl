d8b940204c27b167facc40f75f0c616b71494c49
alfaromartino <alfaromartino@gmail.com>
2025-12-07 00:53:16 -0700

    update

--- assets/PAGES/02f_tuples/codeDownload/region5c.jl
+++
@@ -1,8 +0,0 @@
-using Random; Random.seed!(123)       #setting the seed for reproducibility #hide
-x       = rand(100)
-weights = rand(100) |> (y ->  y ./ sum(y))
-
-temp(x,weights)           = x * weights
-weighted_share(x,weights) = sum(@~ temp.(x,weights))
-
-@btime weighted_share(ref($x), ref($weights)) #hide
